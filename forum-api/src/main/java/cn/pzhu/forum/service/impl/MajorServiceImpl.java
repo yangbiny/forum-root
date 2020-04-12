4eab158a68b6dea65be115aed3093b9a846149f3
impassive <impassive@aliyun.com>
2020-04-12 11:14:26 +0800

    update user info

--- forum-api/src/main/java/cn/pzhu/forum/service/impl/MajorServiceImpl.java
+++ forum-api/src/main/java/cn/pzhu/forum/service/impl/MajorServiceImpl.java
@@ -4,9 +4,11 @@ import cn.pzhu.forum.content.RedisKeyConstant;
 import cn.pzhu.forum.dao.MajorDao;
 import cn.pzhu.forum.entity.Major;
 import cn.pzhu.forum.service.MajorService;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -38,28 +40,29 @@ public class MajorServiceImpl implements MajorService {
         // 存放所有的专业
         List<Major> majorList = null;
         ValueOperations<String, List<Major>> operations = redisTemplate.opsForValue();
-        redisTemplate.expire(key, 1, TimeUnit.MILLISECONDS);
-        redisTemplate.expire(keyList, 1, TimeUnit.MILLISECONDS);
+        redisTemplate.expire(key, 1, TimeUnit.SECONDS);
+        redisTemplate.expire(keyList, 1, TimeUnit.SECONDS);
         Boolean hasKey = redisTemplate.hasKey(key);
         // 有指定学院下的专业
-        if (hasKey != null && hasKey) {
+        /*if (hasKey != null && hasKey) {
             log.info("直接从缓存中查询专业信息");
             List<Major> majors = operations.get(key);
             log.info("查询数据长度：{}", majors == null ? 0 : majors.size());
             return majors;
-        }
+        }*/
         Boolean hasKey1 = redisTemplate.hasKey(keyList);
         List<Major> list = new ArrayList<>();
         // 有所有专业
         if (hasKey1 != null && hasKey1) {
             log.info("从缓存中查询所有的专业信息");
             majorList = operations.get(keyList);
-        } else {
-            majorList = majorDao.list();
-            if (majorList != null) {
-                operations.set(keyList, list);
-            }
         }
+
+        majorList = majorDao.list();
+        if (majorList != null) {
+            operations.set(keyList, list);
+        }
+
         if (majorList != null) {
             // 找出所有专业中，schoolId 等于指定ID的专业并添加到list中
             majorList.stream().filter((major) -> major.getSchoolId() == id).forEach(list::add);
